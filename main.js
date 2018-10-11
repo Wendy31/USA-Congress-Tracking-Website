f8591ae02082d6346f7ed5b9c71005e4efd339b0
Wendy Chung <wendychung@Wendys-MacBook-Pro-2.local>
2018-10-11 18:06:39 +0200

    day 2 of TGIF tables and checkbox

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function createTable() {
 
         var memberInfo = [link, arrayMembers[i].party, arrayMembers[i].state, arrayMembers[i].seniority, arrayMembers[i].votes_with_party_pct];
 
-        
+
         for (var j = 0; j < memberInfo.length; j++) {
             var tableCells = document.createElement("td");
             tableRow.appendChild(tableCells);
@@ -41,47 +41,24 @@ function createTable() {
 
 createTable();
 
+//...........Senate Party Checkbox.............//
+
+function getCheckBoxValue() {
+
+    var tblBody = document.getElementById("tblBody");
+    var rows = tblBody.children;
+    var secondTD = rows[0].childNodes[1];
+    console.log(secondTD);
+
+    var senateCheckedBoxes = document.getElementsByName("partyforsenate"); // pass checkbox names to function
+    for (var n = 0; n < senateCheckedBoxes.length; n++) { //loop over all checkboxes//
+        if (senateCheckedBoxes[n].checked && "R" === senateCheckedBoxes[n].value) {
+            // secondTD.style.display= "none";
+        }
+    }
 
 
+}
 
-//// House Data
-//var arrayHouseMembers = data.results[0].members;
-//
-//function createTable() {
-//
-//
-//    var tbl = document.getElementById("senate-data");
-//    var tblBody = document.getElementById("tblBody");
-//
-//
-//    for (var i = 0; i < arrayHouseMembers.length; i++) {
-//        var tableRow = document.createElement("tr");
-//        tblBody.appendChild(tableRow);
-//
-//
-//        var fullName;
-//
-//        if (arrayHouseMembersMembers[i].middle_name === null) {
-//            fullName = arrayHouseMembersMembers[i].first_name + " " + arrayHouseMembersMembers[i].last_name;
-//        } else {
-//            fullName = arrayHouseMembersMembers[i].first_name + " " + arrayHouseMembersMembers[i].middle_name + " " + arrayHouseMembersMembers[i].last_name;
-//        }
-//
-//        var link = document.createElement("a");
-//        link.setAttribute("href", arrayHouseMembersMembers[i].url);
-//        link.textContent = fullName;
-//
-//        var memberInfo = [link, arrayHouseMembersMembers[i].party, arrayHouseMembersMembers[i].state, arrayHouseMembersMembers[i].seniority, arrayHouseMembersMembers[i].votes_with_party_pct];
-//
-//        
-//        for (var j = 0; j < memberInfo.length; j++) {
-//            var tableCells = document.createElement("td");
-//            tableRow.appendChild(tableCells);
-//            tableCells.append(memberInfo[j]);
-//        }
-//    }
-//    tbl.appendChild(tblBody);
-//    console.log(tbl);
-//}
-//
-//createTable();
+var clickButton = document.getElementById("clickButton");
+clickButton.addEventListener("click", getCheckBoxValue);
