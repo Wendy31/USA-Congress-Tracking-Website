3e0b3dae5bab411acbd9a614e58e4eb62161b825
Wendy Chung <wendychung@Wendys-MacBook-Pro-2.local>
2018-10-15 19:19:06 +0200

    completed checkbox and dropdown menu filters for both senate and house

--- main.js
+++ main.js
@@ -1,10 +1,102 @@
-// Senate Members
+// function hiwendy(arguments,arg1,arg2) {
+//     console.log(arg1) //blah2
+
+//     // return // always end the function execution
+// }
+
+// hiwendy(blah,blah2,blah3)
+
+//...........Senate Party Checkbox.............//
+//
+//function getCheckBoxValue() {
+//
+//    // Logic: 
+//    // 1. Get the value of each checkbox
+//    // 2. For each checked, checkbox, add the value to the array
+//    var arrayCheckedbox = []; // Array of all checked items
+//    var senateCheckedBoxes = document.getElementsByName("partyforsenate"); // pass checkbox names to function
+//    for (var n = 0; n < senateCheckedBoxes.length; n++) { //loop over all checkboxes//
+//        if (senateCheckedBoxes[n].checked) {
+//            arrayCheckedbox.push(senateCheckedBoxes[n].value);
+//        }
+//    }
+//    var tblBody = document.getElementById("tblBody");
+//    var rows = tblBody.getElementsByTagName("tr");
+//
+//    // If arrayCheckBox is empty then display everything
+//    if (arrayCheckedbox.length === 0) {
+//        for (var i = 0; i < rows.length; i++) //loop over all rows
+//        {
+//            rows[i].style.display = "table-row" // and display all table rows
+//        }
+//        return // always ends the function execution
+//    }
+//    // After any checkbox is changed, Loop through all checkboxes and show only those marked as checked
+//    // Loop through all table rows
+//    // check the party columm. If value is in arrayCheckBox then show, else hide
+//
+//    for (var i = 0; i < rows.length; i++) //loop over all rows and get second td
+//    {
+//        var secondTD = rows[i].getElementsByTagName("td")[1].innerText // text inside second cell of a row (repeats thru all rows to get 2nd td)
+//        // console.log(secondTD)  // R
+//        // arrayCheckedeBox = ["R", "D"]
+//        // includes function checks if the given string is inside the array
+//        if (secondTD && arrayCheckedbox.includes(secondTD)) //if secondTD (from table) & array both contain (secondTD)
+//        {
+//            rows[i].style.display = "table-row";
+//        } else {
+//            rows[i].style.display = "none";
+//        }
+//    }
+//}
+//// JS lets you execute code when events are detected. "Click" checkbox to run function.
+//document.getElementById("clickR").addEventListener("click", getCheckBoxValue);
+//document.getElementById("clickD").addEventListener("click", getCheckBoxValue);
+//document.getElementById("clickI").addEventListener("click", getCheckBoxValue);
+//
+//
+//// Fitler table by state name //
+//
+//function filterByStates() {
+//    // Get the Table
+//    var tableData = document.getElementById("tblBody"); // tablebody from HTML to JS
+//    var rows = tblBody.getElementsByTagName("tr"); // get all rows 
+//    // get thirdTD column "state" by looping all the rows first
+//
+//    // get the dropdown menu
+//    var dropmenu = document.getElementById("statedropdown");
+//    // get the state thats selected
+//    var selectedStates = dropmenu[dropmenu.selectedIndex].value;
+//    // if selectedStates Value === to table td3 then show tr by looping and show all relevant TRs
+//
+//    // if item in dropdown includes TD3 then show TR
+//    for (var i = 0; i < rows.length; i++) { // loop over all rows 
+//        var thirdTD = rows[i].getElementsByTagName("td")[2].innerText; // to get thirdTD
+//        if (thirdTD && selectedStates.includes(thirdTD)) { //if thirdTD (from table) & dropDown contains (thirdTD)
+//            rows[i].style.display = "table-row"; //display all rows
+//        } else {
+//            rows[i].style.display = "none"; // else hide the rows
+//        }
+//    }
+//
+//    // then show all rows when "ALL" from HTML is selected 
+//    var all = document.getElementsByTagName("option")[0].value; // get value from HTML to JS (why .firstChild returns "text") 
+//    if (selectedStates.includes(all)) { // if "All" selected, display all rows. 
+//        for (var i = 0; i < rows.length; i++) { // loop all rows
+//            rows[i].style.display = "table-row"; // and display all
+//        }
+//        return;
+//    }
+//}
+//document.getElementById("statedropdown").addEventListener("change", filterByStates);
+//
+
+
+//................ Table of Senate Members................//
 
 var arrayMembers = data.results[0].members;
 
 function createTable() {
-
-
     var tbl = document.getElementById("senate-data");
     var tblBody = document.getElementById("tblBody");
 
@@ -12,7 +104,6 @@ function createTable() {
     for (var i = 0; i < arrayMembers.length; i++) {
         var tableRow = document.createElement("tr");
         tblBody.appendChild(tableRow);
-
 
         var fullName;
 
@@ -38,27 +129,113 @@ function createTable() {
     tbl.appendChild(tblBody);
     console.log(tbl);
 }
-
 createTable();
 
-//...........Senate Party Checkbox.............//
 
-function getCheckBoxValue() {
+//................. Populate state names to dropdown menu ..................//
 
-    var tblBody = document.getElementById("tblBody");
-    var rows = tblBody.children;
-    var secondTD = rows[0].childNodes[1];
-    console.log(secondTD);
+function populateStateDropdown() {
+    // Get the Table
+    var tableData = document.getElementById("tblBody"); // tablebody from HTML to JS
+    var rows = tblBody.getElementsByTagName("tr"); // get all rows 
+    var arrayStates = []; // creating variable arrayStates and setting it to an empty array
 
+    // get thirdTD column "state" by looping all the rows first
+    for (var i = 0; i < rows.length; i++) {
+        var thirdTD = rows[i].getElementsByTagName("td")[2].innerText //text inside third cell of a row
+        // to not add same states inside the array we check if arrayStates has states inside already
+        if (!arrayStates.includes(thirdTD)) { // if not inside the array, we want to add it. Stops dublicates.
+            arrayStates.push(thirdTD); // put each state in the array 
+            arrayStates.sort(); // sort in alphabetical order
+        }
+    }
+    // console.log(arrayStates) // list of states 
+    // get the dropdown from HTML
+    var dropdown = document.getElementById("statedropdown"); // dropdown menu from HTML to JS
+    // Populate the menu by adding an option element for each state in arrayStates
+    for (var i = 0; i < arrayStates.length; i++) {
+        // take a state from arraystates
+        var state = arrayStates[i]
+        // add it to the dropdown element as an <option>
+        var option = document.createElement("option"); //created option element
+        // add the state into the option as a value
+        option.setAttribute("value", state) // added array states into option
+        option.innerText = state // added states as text to option
+        // add the option to the dropdown element
+        dropdown.add(option); // no array of states anymore, all inside 
+        //        <select><option></option></select> as text
+    }
+    // end goal is to have a select that looks something like this
+    // <select>
+    //     <option></option>
+    //     <option value="All">All</option>
+    //     <option value="Texas">Texas</option>
+    //     <option value="Hawaii">Hawaii</option>
+    // </select>
+}
+populateStateDropdown();
+
+
+//......................... Combine both Checkbox and Dropdown Menu filters ....................... //
+/* LOGIC:
+ Goal: when checkbox checked and state name selected display rows that contain both
+ 1. function getPartyAndState
+ 2. Get table, tr and td2
+ 2. if party.checked === secondTD && selectedStates.includes(party.checked) --> display rows
+ 3. loop all rows and display relevant rows 
+ */
+
+function getPartyAndState() {
+    var dropmenu = document.getElementById("statedropdown").value;
+
+
+    // get checkbox to filter table
+    var arrayCheckedbox = []; // Array of all checked items
     var senateCheckedBoxes = document.getElementsByName("partyforsenate"); // pass checkbox names to function
     for (var n = 0; n < senateCheckedBoxes.length; n++) { //loop over all checkboxes//
-        if (senateCheckedBoxes[n].checked && "R" === senateCheckedBoxes[n].value) {
-            // secondTD.style.display= "none";
+        if (senateCheckedBoxes[n].checked) {
+            arrayCheckedbox.push(senateCheckedBoxes[n].value);
         }
     }
 
+    var tblBody = document.getElementById("tblBody");
+    var rows = tblBody.getElementsByTagName("tr");
 
+    // After any checkbox is changed, Loop through all checkboxes and show only those marked as checked
+    // Loop through all table rows
+    // check the party columm. If value is in arrayCheckBox then show, else hide
+
+    for (var i = 0; i < rows.length; i++) //loop over all rows and get second td
+    {
+        var thirdTD = rows[i].getElementsByTagName("td")[2].innerText
+        var secondTD = rows[i].getElementsByTagName("td")[1].innerText // text inside second cell of a row (repeats thru all rows to get 2nd td)
+        // console.log(secondTD)  // R
+        // arrayCheckedeBox = ["R", "D"]
+        // includes function checks if the given string is inside the array
+        if (arrayCheckedbox.includes(secondTD) && (dropmenu.includes(thirdTD) || dropmenu.includes("All"))) //if secondTD (from table) & array both contain (secondTD)
+        {
+            rows[i].style.display = "table-row";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+    // if arrayCheckBox is empty then display everything
+    if (arrayCheckedbox.length === 0) { // now array is empty
+        for (var i = 0; i < rows.length; i++) //loop over all rows
+        {
+            var thirdTD = rows[i].getElementsByTagName("td")[2].innerText;
+
+            if (dropmenu.includes(thirdTD) || dropmenu.includes("All")) {
+                rows[i].style.display = "table-row";
+            } else {
+                rows[i].style.display = "none";
+            }
+        }
+    }
 }
 
-var clickButton = document.getElementById("clickButton");
-clickButton.addEventListener("click", getCheckBoxValue);
+// JS lets you execute code when events are detected. "Click" checkbox to run function.
+document.getElementById("clickR").addEventListener("click", getPartyAndState);
+document.getElementById("clickD").addEventListener("click", getPartyAndState);
+document.getElementById("clickI").addEventListener("click", getPartyAndState);
+document.getElementById("statedropdown").addEventListener("change", getPartyAndState);
